b95a0247a06a02195d0774aec4c3970fb16685f7
Painfulaintit <disisapain@gmail.com>
2015-05-19 22:39:03 +0200

    title added

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
-echo "No akkor ez már megy vazze??";
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Yo mon Facebook fo' da chills</title>
+</head>
+<body>
+<h1>Yo mon Facebook fo' da chills</h1>
+
+</body>
+</html>
